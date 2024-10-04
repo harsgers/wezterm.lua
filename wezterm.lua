c6df143fd83b62688462d7e9bc060636f5da46e4
lfg-hjg <harrisongerstenlauer@gmail.com>
2024-10-04 09:39:42 -0400

    idk

--- wezterm.lua
+++ wezterm.lua
@@ -3,16 +3,14 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
-
 -- COLOR SCHEME & FONT
-config.color_scheme = "Nightfly (Gogh)"
 config.font = wezterm.font("JetBrains Mono")
+config.color_scheme = "Nightfly (Gogh)"
 
 --TAB BAR
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
--- and finally, return the configuration to wezterm
+-- KEY BINDINGS
+
 return config
