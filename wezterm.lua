e6dd22ef8365b004571e18e72b5676e95812615e
harsgers <harrison.gerstenlauer@gmail.com>
2024-11-21 10:38:09 -0500

    darken inactive window panes

--- wezterm.lua
+++ wezterm.lua
@@ -12,5 +12,8 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
 -- KEY BINDINGS
-
+config.inactive_pane_hsb = {
+	saturation = 0.8,
+	brightness = 0.4,
+}
 return config
